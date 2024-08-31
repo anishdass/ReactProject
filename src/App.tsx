c5619e1c94872bad41bab5cb0d98d3f1c85b48f3
anishdass <anishdassatoffice@gmail.com>
2024-08-31 09:43:47 +0100

    Worked

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
@@ -8,16 +10,24 @@ function App() {
     console.log(item);
   };
 
+  const [alertVisibility, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Alert>
+      {/* <Alert>
         Hello <span>World</span>
-      </Alert>
+      </Alert> */}
       {/* <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
       /> */}
+      {alertVisibility && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="dark" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
